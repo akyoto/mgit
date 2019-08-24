b3088bd406c55cdb6a3f3d91c4ebf947f4b7eac1
Eduard Urbach <e.urbach@gmail.com>
2019-08-24 15:21:10 +0900

    Use akyoto/assert for tests

--- go.mod
+++ go.mod
@@ -3,9 +3,7 @@ module github.com/akyoto/mgit
 go 1.12
 
 require (
-	github.com/akyoto/color v1.8.4
-	github.com/akyoto/ignore v1.0.2
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/stretchr/testify v1.3.0
-	golang.org/x/sys v0.0.0-20190523142557-0e01d883c5c5 // indirect
+	github.com/akyoto/assert v0.1.3
+	github.com/akyoto/color v1.8.7
+	github.com/akyoto/ignore v1.0.3
 )
--- parse/Args_test.go
+++ parse/Args_test.go
@@ -1,21 +1,22 @@
 package parse_test
 
 import (
-	"github.com/akyoto/mgit/parse"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/akyoto/assert"
+	"github.com/akyoto/mgit/parse"
 )
 
 func TestSimpleArgs(t *testing.T) {
 	args := parse.Args("git status")
-	assert.Len(t, args, 2)
+	assert.Equal(t, len(args), 2)
 	assert.Equal(t, args[0], "git")
 	assert.Equal(t, args[1], "status")
 }
 
 func TestAdvancedArgs(t *testing.T) {
 	args := parse.Args("git commit -m \"My update message\"")
-	assert.Len(t, args, 4)
+	assert.Equal(t, len(args), 4)
 	assert.Equal(t, args[0], "git")
 	assert.Equal(t, args[1], "commit")
 	assert.Equal(t, args[2], "-m")
@@ -24,7 +25,7 @@ func TestAdvancedArgs(t *testing.T) {
 
 func TestWeirdArgs(t *testing.T) {
 	args := parse.Args("\"git\" \"\" \"oh\" \"oh noes\" \"this must not break\"")
-	assert.Len(t, args, 5)
+	assert.Equal(t, len(args), 5)
 	assert.Equal(t, args[0], "git")
 	assert.Equal(t, args[1], "")
 	assert.Equal(t, args[2], "oh")
